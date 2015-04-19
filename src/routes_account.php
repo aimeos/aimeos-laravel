d726551d3a3fd07b471ebf267cde057c957814f3
Andreas <andi@aimeos.org>
2015-04-19 20:04:05 +0200

    Fixed routes

--- src/routes_account.php
+++ src/routes_account.php
@@ -2,13 +2,13 @@
 
 Route::match( array( 'GET', 'POST' ), 'myaccount', array(
 	'as' => 'aimeos_shop_account',
-	'uses' => 'Aimeos\Shop\Controller\AccountController@indexAction'
+	'uses' => '\Aimeos\Shop\Controller\AccountController@indexAction'
 ));
 Route::match( array( 'GET', 'POST' ), 'myaccount/favorite/{fav_action?}/{fav_id?}/{d_prodid?}/{d_name?}/{l_pos?}', array(
 	'as' => 'aimeos_shop_account_favorite',
-	'uses' => 'Aimeos\Shop\Controller\AccountController@indexAction'
+	'uses' => '\Aimeos\Shop\Controller\AccountController@indexAction'
 ));
 Route::match( array( 'GET', 'POST' ), 'myaccount/watch/{wat_action?}/{wat_id?}/{d_prodid?}/{d_name?}/{l_pos?}', array(
 	'as' => 'aimeos_shop_account_watch',
-	'uses' => 'Aimeos\Shop\Controller\AccountController@indexAction'
+	'uses' => '\Aimeos\Shop\Controller\AccountController@indexAction'
 ));
