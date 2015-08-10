59f7ce3872ad40fd4ef805317f01eaf214e1c3b2
Andreas <andi@aimeos.org>
2015-08-10 22:22:56 +0200

    Added label column to users table

---
+++ src/database/migrations/2015_08_10_000000_aimeos_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AimeosUsersLabel extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('label', 255);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn(array('label'));
+		});
+	}
+}
