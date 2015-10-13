ff8a2a80556b4704602c779af3a03f25d042cd02
Andreas <andi@aimeos.org>
2015-10-13 10:19:07 +0200

    Use MySQL catalog index implementation

--- src/config/shop.php
+++ src/config/shop.php
@@ -45,6 +45,25 @@ return array(
 				'name' => 'Laravel',
 			),
 		),
+		'catalog' => array(
+			'manager' => array(
+				'index' => array(
+					'name' => 'MySQL',
+					'attribute' => array(
+						'name' => 'MySQL',
+					),
+					'catalog' => array(
+						'name' => 'MySQL',
+					),
+					'price' => array(
+						'name' => 'MySQL',
+					),
+					'text' => array(
+						'name' => 'MySQL',
+					),
+				),
+			),
+		),
 	),
 
 	'client' => array(
