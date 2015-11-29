55b48c8986025c46d1460bdd1ac065caa2336823
Andreas <andi@aimeos.org>
2015-11-29 22:54:07 +0100

    Better unit test for support service

--- tests/Base/SupportTest.php
+++ tests/Base/SupportTest.php
@@ -4,6 +4,22 @@ class SupportTest extends AimeosTestAbstract
 {
 	public function testCheckGroup()
 	{
-		$this->assertFalse( $this->app->make('\Aimeos\Shop\Base\Support')->checkGroup( -1, 'admin' ) );
+		$context = $this->app->make('\Aimeos\Shop\Base\Context');
+		$ctx = $context->get(false);
+
+		$localeManager = \Aimeos\MShop\Locale\Manager\Factory::createManager( $ctx );
+		$ctx->setLocale( $localeManager->bootstrap( 'unittest', '', '', false ) );
+
+		$ctxMock = $this->getMockBuilder('\Aimeos\Shop\Base\Context')
+			->disableOriginalConstructor()
+			->setMethods( array('get') )
+			->getMock();
+
+		$ctxMock->expects( $this->once() )->method('get')
+			->will( $this->returnValue($ctx) );
+
+		$object = new \Aimeos\Shop\Base\Support($ctxMock);
+
+		$this->assertFalse( $object->checkGroup( -1, 'admin' ) );
 	}
 }
