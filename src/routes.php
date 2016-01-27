0e8d16862ebccced7c900f85763f1cd1db8d366b
Andreas <andi@aimeos.org>
2016-01-28 00:08:43 +0100

    Use "web" middleware required by Laravel 5.2

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,16 @@
 <?php
 
-Route::match( array( 'GET' ), 'admin', array(
-	'as' => 'aimeos_shop_admin',
-	'uses' => 'Aimeos\Shop\Controller\AdminController@indexAction'
-));
+Route::group(config('shop.routes.login', ['middleware' => ['web']]), function() {
+
+	Route::match( array( 'GET' ), 'admin', array(
+		'as' => 'aimeos_shop_admin',
+		'uses' => 'Aimeos\Shop\Controller\AdminController@indexAction'
+	));
+
+});
 
 
-Route::group(config('shop.routes.admin', ['middleware' => 'auth']), function() {
+Route::group(config('shop.routes.admin', ['middleware' => ['web', 'auth']]), function() {
 
 	Route::match( array( 'GET' ), 'extadm', array(
 		'as' => 'aimeos_shop_extadm',
@@ -88,16 +92,18 @@ Route::group(config('shop.routes.admin', ['middleware' => 'auth']), function() {
 });
 
 
-Route::group(config('shop.routes.account', ['middleware' => 'auth']), function() {
+Route::group(config('shop.routes.account', ['middleware' => ['web', 'auth']]), function() {
 
 	Route::match( array( 'GET', 'POST' ), 'myaccount', array(
 		'as' => 'aimeos_shop_account',
 		'uses' => 'Aimeos\Shop\Controller\AccountController@indexAction'
 	));
+
 	Route::match( array( 'GET', 'POST' ), 'myaccount/favorite/{fav_action?}/{fav_id?}/{d_prodid?}/{d_name?}/{l_pos?}', array(
 		'as' => 'aimeos_shop_account_favorite',
 		'uses' => 'Aimeos\Shop\Controller\AccountController@indexAction'
 	));
+
 	Route::match( array( 'GET', 'POST' ), 'myaccount/watch/{wat_action?}/{wat_id?}/{d_prodid?}/{d_name?}/{l_pos?}', array(
 		'as' => 'aimeos_shop_account_watch',
 		'uses' => 'Aimeos\Shop\Controller\AccountController@indexAction'
@@ -106,58 +112,77 @@ Route::group(config('shop.routes.account', ['middleware' => 'auth']), function()
 });
 
 
-Route::group(config('shop.routes.default', []), function() {
+Route::group(config('shop.routes.default', ['middleware' => ['web']]), function() {
+
+	Route::match( array( 'GET', 'POST' ), 'count', array(
+		'as' => 'aimeos_shop_count',
+		'uses' => 'Aimeos\Shop\Controller\CatalogController@countAction'
+	));
+
+	Route::match( array( 'GET', 'POST' ), 'detail/{d_prodid}/{d_name?}/{l_pos?}', array(
+		'as' => 'aimeos_shop_detail',
+		'uses' => 'Aimeos\Shop\Controller\CatalogController@detailAction'
+	));
+
+	Route::match( array( 'GET', 'POST' ), 'detail/pin/{pin_action?}/{pin_id?}/{d_prodid?}/{d_name?}/{l_pos?}', array(
+		'as' => 'aimeos_shop_session_pinned',
+		'uses' => 'Aimeos\Shop\Controller\CatalogController@detailAction'
+	));
+
+	Route::match( array( 'GET', 'POST' ), 'list', array(
+		'as' => 'aimeos_shop_list',
+		'uses' => 'Aimeos\Shop\Controller\CatalogController@listAction'
+	));
+
+	Route::match( array( 'GET', 'POST' ), 'suggest', array(
+		'as' => 'aimeos_shop_suggest',
+		'uses' => 'Aimeos\Shop\Controller\CatalogController@suggestAction'
+	));
+
+	Route::match( array( 'GET', 'POST' ), 'stock', array(
+		'as' => 'aimeos_shop_stock',
+		'uses' => 'Aimeos\Shop\Controller\CatalogController@stockAction'
+	));
 
 	Route::match( array( 'GET', 'POST' ), 'basket', array(
 		'as' => 'aimeos_shop_basket',
 		'uses' => 'Aimeos\Shop\Controller\BasketController@indexAction'
 	));
 
-	Route::match( array( 'GET', 'POST' ), 'count', array(
-		'as' => 'aimeos_shop_count',
-		'uses' => 'Aimeos\Shop\Controller\CatalogController@countAction'
-	));
-	Route::match( array( 'GET', 'POST' ), 'detail/{d_prodid}/{d_name?}/{l_pos?}', array(
-		'as' => 'aimeos_shop_detail',
-		'uses' => 'Aimeos\Shop\Controller\CatalogController@detailAction'
-	));
-	Route::match( array( 'GET', 'POST' ), 'detail/pin/{pin_action?}/{pin_id?}/{d_prodid?}/{d_name?}/{l_pos?}', array(
-		'as' => 'aimeos_shop_session_pinned',
-		'uses' => 'Aimeos\Shop\Controller\CatalogController@detailAction'
-	));
-	Route::match( array( 'GET', 'POST' ), 'list', array(
-		'as' => 'aimeos_shop_list',
-		'uses' => 'Aimeos\Shop\Controller\CatalogController@listAction'
-	));
-	Route::match( array( 'GET', 'POST' ), 'suggest', array(
-		'as' => 'aimeos_shop_suggest',
-		'uses' => 'Aimeos\Shop\Controller\CatalogController@suggestAction'
-	));
-	Route::match( array( 'GET', 'POST' ), 'stock', array(
-		'as' => 'aimeos_shop_stock',
-		'uses' => 'Aimeos\Shop\Controller\CatalogController@stockAction'
-	));
-
 	Route::match( array( 'GET', 'POST' ), 'checkout/{c_step?}', array(
 		'as' => 'aimeos_shop_checkout',
 		'uses' => 'Aimeos\Shop\Controller\CheckoutController@indexAction'
 	));
+
+});
+
+
+Route::group(config('shop.routes.confirm', ['middleware' => ['web']]), function() {
+
 	Route::match( array( 'GET', 'POST' ), 'confirm', array(
 		'as' => 'aimeos_shop_confirm',
 		'uses' => 'Aimeos\Shop\Controller\CheckoutController@confirmAction'
 	));
+
+});
+
+
+Route::group(config('shop.routes.update', []), function() {
+
 	Route::match( array( 'GET', 'POST' ), 'update', array(
 		'as' => 'aimeos_shop_update',
 		'uses' => 'Aimeos\Shop\Controller\CheckoutController@updateAction'
 	));
 
-	Route::get('terms', array(
-		'as' => 'aimeos_shop_terms',
-		'uses' => 'Aimeos\Shop\Controller\PageController@termsAction'
-	));
-	Route::get('privacy', array(
-		'as' => 'aimeos_shop_privacy',
-		'uses' => 'Aimeos\Shop\Controller\PageController@privacyAction'
-	));
-
 });
+
+
+Route::get('terms', array(
+	'as' => 'aimeos_shop_terms',
+	'uses' => 'Aimeos\Shop\Controller\PageController@termsAction'
+));
+
+Route::get('privacy', array(
+	'as' => 'aimeos_shop_privacy',
+	'uses' => 'Aimeos\Shop\Controller\PageController@privacyAction'
+));
