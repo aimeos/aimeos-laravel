21f2d4fa3b420be1bc4d86d8eb0dd129944f3270
Andreas <andi@aimeos.org>
2016-03-15 19:41:46 +0100

    Added missing view for image uploads

--- src/Aimeos/Shop/Controller/ExtadmController.php
+++ src/Aimeos/Shop/Controller/ExtadmController.php
@@ -110,6 +110,7 @@ class ExtadmController extends AdminController
 		$cntlPaths = $aimeos->getCustomPaths( 'controller/extjs' );
 
 		$context = app( '\Aimeos\Shop\Base\Context' )->get( false );
+		$context->setView( app( '\Aimeos\Shop\Base\View' )->create( $context->getConfig(), array() ) );
 		$context = $this->setLocale( $context );
 
 		$controller = new \Aimeos\Controller\ExtJS\JsonRpc( $context, $cntlPaths );
