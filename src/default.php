26f06561ba7c590c2738ea937ffbaf1f29f3c624
Aimeos <aimeos@aimeos.org>
2016-06-19 23:52:52 +0200

    Socket configuration isn't used

--- src/default.php
+++ src/default.php
@@ -25,7 +25,6 @@ return array(
 			'adapter' => env('DB_CONNECTION', 'mysql'),
 			'host' => env('DB_HOST', 'localhost'),
 			'port' => env('DB_PORT', ''),
-			'socket' => '',
 			'database' => env('DB_DATABASE', 'laravel'),
 			'username' => env('DB_USERNAME', 'root'),
 			'password' => env('DB_PASSWORD', ''),
