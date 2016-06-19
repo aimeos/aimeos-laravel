275acc8dc4572602f3fe36e32b7a70a008a78040
Aimeos <aimeos@aimeos.org>
2016-06-19 23:50:28 +0200

    Improved example configuration

--- src/config/shop.php
+++ src/config/shop.php
@@ -3,35 +3,36 @@
 return array(
 
 	'routes' => array(
-		# 'login' => array('middleware' => ['web']),
-		# 'admin' => array('middleware' => ['web', 'auth']),
-		# 'account' => array('middleware' => ['web', 'auth']),
-		# 'default' => array('middleware' => ['web']),
-		# 'confirm' => array('middleware' => ['web']),
-		# 'update' => array(),
+		// 'login' => array('middleware' => ['web']),
+		// 'admin' => array('middleware' => ['web', 'auth']),
+		// 'account' => array('middleware' => ['web', 'auth']),
+		// 'default' => array('middleware' => ['web']),
+		// 'confirm' => array('middleware' => ['web']),
+		// 'update' => array(),
 	),
 
 	'page' => array(
-		# 'account-index' => array( 'account/history','account/favorite','account/watch','basket/mini','catalog/session' ),
-		# 'basket-index' => array( 'basket/standard','basket/related' ),
-		# 'catalog-count' => array( 'catalog/count' ),
-		# 'catalog-detail' => array( 'basket/mini','catalog/stage','catalog/detail','catalog/session' ),
-		# 'catalog-list' => array( 'basket/mini','catalog/filter','catalog/stage','catalog/lists' ),
-		# 'catalog-stock' => array( 'catalog/stock' ),
-		# 'catalog-suggest' => array( 'catalog/suggest' ),
-		# 'checkout-confirm' => array( 'checkout/confirm' ),
-		# 'checkout-index' => array( 'checkout/standard' ),
-		# 'checkout-update' => array( 'checkout/update'),
+		// 'account-index' => array( 'account/history','account/favorite','account/watch','basket/mini','catalog/session' ),
+		// 'basket-index' => array( 'basket/standard','basket/related' ),
+		// 'catalog-count' => array( 'catalog/count' ),
+		// 'catalog-detail' => array( 'basket/mini','catalog/stage','catalog/detail','catalog/session' ),
+		// 'catalog-list' => array( 'basket/mini','catalog/filter','catalog/stage','catalog/lists' ),
+		// 'catalog-stock' => array( 'catalog/stock' ),
+		// 'catalog-suggest' => array( 'catalog/suggest' ),
+		// 'checkout-confirm' => array( 'checkout/confirm' ),
+		// 'checkout-index' => array( 'checkout/standard' ),
+		// 'checkout-update' => array( 'checkout/update'),
 	),
 
 	'resource' => array(
 		'db' => array(
-			# 'adapter' => 'mysql',
-			# 'host' => env('DB_HOST', 'localhost'),
-			# 'port' => env('DB_PORT', ''),
-			# 'database' => env('DB_DATABASE', 'laravel'),
-			# 'username' => env('DB_USERNAME', 'root'),
-			# 'password' => env('DB_PASSWORD', ''),
+			// 'adapter' => env('DB_CONNECTION', 'mysql'),
+			// 'host' => env('DB_HOST', 'localhost'),
+			// 'port' => env('DB_PORT', ''),
+			// 'socket' => '',
+			// 'database' => env('DB_DATABASE', 'laravel'),
+			// 'username' => env('DB_USERNAME', 'root'),
+			// 'password' => env('DB_PASSWORD', ''),
 		),
 	),
 
@@ -39,10 +40,10 @@ return array(
 		'html' => array(
 			'common' => array(
 				'content' => array(
-					# 'baseurl' => '/',
+					// 'baseurl' => '/',
 				),
 				'template' => array(
-					# 'baseurl' => 'packages/aimeos/shop/elegance',
+					// 'baseurl' => 'packages/aimeos/shop/elegance',
 				),
 			),
 		),
