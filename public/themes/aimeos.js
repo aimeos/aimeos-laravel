98948af112852ef4ac7e0d6046e13136b943722a
Andreas <andi@aimeos.org>
2017-01-01 15:15:37 +0100

    Update from core

--- public/themes/aimeos.js
+++ public/themes/aimeos.js
@@ -3,7 +3,7 @@
  *
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
  * @copyright Metaways Infosystems GmbH, 2012
- * @copyright Aimeos (aimeos.org), 2014
+ * @copyright Aimeos (aimeos.org), 2014-2016
  */
 
 
