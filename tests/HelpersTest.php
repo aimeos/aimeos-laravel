5eaebb3c230f27b415f4ffa8574e6959d2de74a5
Andreas <andi@aimeos.org>
2017-01-16 00:50:12 +0100

    Translation helper function is hard to test

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -7,16 +7,4 @@ class HelpersTest extends AimeosTestAbstract
 	{
 		$this->assertEquals( 'notexisting', aiconfig( 'not/exists', 'notexisting' ) );
 	}
-
-
-	public function testAitrans()
-	{
-		$this->assertEquals( '1 not exists', aitrans( '%1$d not exists', array( 1 ) ) );
-	}
-
-
-	public function testAitransplural()
-	{
-		$this->assertEquals( '2 not exist', aitransplural( '%1$d not exists', '%1$d not exist', 2, array( 2 ) ) );
-	}
 }
