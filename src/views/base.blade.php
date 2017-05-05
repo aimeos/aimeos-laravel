c2956b02703c162aa95208f55c2acb1eeeb4827e
Andreas <andi@aimeos.org>
2017-05-05 18:24:43 +0200

    Add polyfill.js to iron out missing features in browsers

--- src/views/base.blade.php
+++ src/views/base.blade.php
@@ -6,6 +6,7 @@
 @stop
 
 @section('aimeos_scripts')
+	<script type="text/javascript" src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
     <script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/jquery-ui.custom.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/aimeos.js') }}"></script>
     <script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/elegance/aimeos.js') }}"></script>
