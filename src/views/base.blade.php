136e33830c060bafb0de5fc4dc5bbbe6c75aa01f
Silvan <silvan@dev7.ch>
2018-04-08 09:37:36 +0200

    Update base.blade.php

--- src/views/base.blade.php
+++ src/views/base.blade.php
@@ -1,12 +1,12 @@
 @extends('app')
 
 @section('aimeos_styles')
-    <link type="text/css" rel="stylesheet" href="{{ asset('packages/aimeos/shop/themes/elegance/aimeos.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ asset('packages/aimeos/shop/themes/elegance/aimeos.css') }}" />
 @stop
 
 @section('aimeos_scripts')
 	<script type="text/javascript" src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-    <script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/jquery-ui.custom.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/aimeos.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/elegance/aimeos.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/jquery-ui.custom.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/aimeos.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('packages/aimeos/shop/themes/elegance/aimeos.js') }}"></script>
 @stop
