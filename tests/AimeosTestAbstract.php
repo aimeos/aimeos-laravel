aa519b5d752365a88fd84c04f2ac84de0d4025ea
Andreas <andi@aimeos.org>
2018-10-09 09:41:44 +0200

    Use database settings from Laravel config/database.php

--- tests/AimeosTestAbstract.php
+++ tests/AimeosTestAbstract.php
@@ -12,10 +12,12 @@ class AimeosTestAbstract extends Orchestra\Testbench\BrowserKit\TestCase
 		$app['config']->set('database.default', 'mysql');
 		$app['config']->set('database.connections.mysql', [
 			'driver' => 'mysql',
-			'host' => env('DB_HOST', 'localhost'),
+			'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
 			'database' => env('DB_DATABASE', 'laravel'),
 			'username' => env('DB_USERNAME', 'root'),
 			'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
 			'collation' => 'utf8_unicode_ci',
 		]);
 
