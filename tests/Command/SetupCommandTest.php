2e1343610b79a8558b21c24c3b03dfbd07acdc0d
Andreas <andi@aimeos.org>
2019-01-08 11:02:14 +0100

    Removed setup rollback and clean tests due to travis-ci MySQL version

--- tests/Command/SetupCommandTest.php
+++ tests/Command/SetupCommandTest.php
@@ -2,20 +2,6 @@
 
 class SetupCommandTest extends AimeosTestAbstract
 {
-	public function testSetupCommandRollback()
-	{
-		$args = array('site' => 'unittest', 'tplsite' => 'unittest', '--task' => 'TablesCreateMShop', '--action' => 'rollback');
-		$this->assertEquals(0, $this->artisan('aimeos:setup', $args));
-	}
-
-
-	public function testSetupCommandClean()
-	{
-		$args = array('site' => 'unittest', 'tplsite' => 'unittest', '--action' => 'clean');
-		$this->assertEquals(0, $this->artisan('aimeos:setup', $args));
-	}
-
-
 	public function testSetupCommand()
 	{
 		$args = array('site' => 'unittest', 'tplsite' => 'unittest', '--option' => 'setup/default/demo:0');
