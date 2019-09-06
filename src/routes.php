cec86c45707b588fe6e039c8417afa3c0c19fa87
Andreas <andi@aimeos.org>
2019-09-06 19:55:59 +0200

    Make ID for JQAdm delete URL optional

--- src/routes.php
+++ src/routes.php
@@ -32,7 +32,7 @@ if(($conf = config('shop.routes.jqadm', ['prefix' => 'admin/{site}/jqadm', 'midd
 			'uses' => 'Aimeos\Shop\Controller\JqadmController@createAction'
 		))->where( ['site' => '[a-z0-9\.\-]+'] )->where( array( 'resource' => '[a-z\/]+' ) );
 
-		Route::match( array( 'GET', 'POST' ), 'delete/{resource}/{id}', array(
+		Route::match( array( 'GET', 'POST' ), 'delete/{resource}/{id?}', array(
 			'as' => 'aimeos_shop_jqadm_delete',
 			'uses' => 'Aimeos\Shop\Controller\JqadmController@deleteAction'
 		))->where( ['site' => '[a-z0-9\.\-]+'] )->where( array( 'resource' => '[a-z\/]+' ) );
