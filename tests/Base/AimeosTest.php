e359c57cb9271f336f78804813517015845d9c58
Andreas <andi@aimeos.org>
2020-01-24 18:09:42 +0100

    Revert "Use PHPUnit 7.x/8.x"
    This reverts commit 0bcae305cc85fba62f3f76a08c3f47d48d927c31.

--- tests/Base/AimeosTest.php
+++ tests/Base/AimeosTest.php
@@ -12,6 +12,6 @@ class AimeosTest extends AimeosTestAbstract
 	public function testGetVersion()
 	{
 		$object = $this->app->make( '\Aimeos\Shop\Base\Aimeos' );
-		$this->assertIsString( $object->getVersion() );
+		$this->assertInternalType( 'string', $object->getVersion() );
 	}
 }
