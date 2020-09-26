115852e5e34a53731bddc220f47619733bfe9661
Andreas <andi@aimeos.org>
2020-09-26 11:47:07 +0200

    Strip down layout view for tests

--- tests/fixtures/views/app.blade.php
+++ tests/fixtures/views/app.blade.php
@@ -1,79 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="csrf-token" content="{{ csrf_token() }}" />
-@yield('aimeos_header')
-	<title>Laravel</title>
+	<head>
+		<meta name="csrf-token" content="{{ csrf_token() }}" />
+		@yield('aimeos_header')
+		<title>Laravel</title>
+		@yield('aimeos_styles')
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			@yield('aimeos_head')
+		</nav>
+		@yield('aimeos_nav')
+		@yield('aimeos_stage')
+		@yield('aimeos_body')
+		@yield('aimeos_aside')
+		@yield('content')
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/css/app.css" rel="stylesheet">
-@yield('aimeos_styles')
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-	<nav class="navbar navbar-default">
-@yield('aimeos_head')
-	<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="/">Home</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li>
-						<li><a href="/auth/register">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="/auth/logout">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-    <div class="col-xs-12">
-@yield('aimeos_nav')
-@yield('aimeos_stage')
-@yield('aimeos_body')
-@yield('aimeos_aside')
-@yield('content')
-	</div>
-
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-@yield('aimeos_scripts')
-	<script type="text/javascript">
-		$.ajaxSetup({
-		    headers: {
-		        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-		    }
-		});
-	</script>
+		@yield('aimeos_scripts')
 	</body>
 </html>
