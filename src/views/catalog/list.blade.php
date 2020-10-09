12244d336af45b3218d9d264a4f9816a6c142477
Andreas <andi@aimeos.org>
2020-10-09 08:56:42 +0200

    Added component output if it exists There's no need to adapt the files when adding existing components

--- src/views/catalog/list.blade.php
+++ src/views/catalog/list.blade.php
@@ -1,19 +1,29 @@
 @extends('shop::base')
 
 @section('aimeos_header')
-    <?= $aiheader['basket/mini'] ?>
-    <?= $aiheader['catalog/filter'] ?>
-    <?= $aiheader['catalog/lists'] ?>
+    <?= $aiheader['basket/mini'] ?? '' ?>
+    <?= $aiheader['catalog/filter'] ?? '' ?>
+    <?= $aiheader['catalog/search'] ?? '' ?>
+    <?= $aiheader['catalog/tree'] ?? '' ?>
+    <?= $aiheader['catalog/price'] ?? '' ?>
+    <?= $aiheader['catalog/supplier'] ?? '' ?>
+    <?= $aiheader['catalog/attribute'] ?? '' ?>
+    <?= $aiheader['catalog/lists'] ?? '' ?>
 @stop
 
 @section('aimeos_head')
-    <?= $aibody['basket/mini'] ?>
+    <?= $aibody['basket/mini'] ?? '' ?>
 @stop
 
 @section('aimeos_nav')
-    <?= $aibody['catalog/filter'] ?>
+    <?= $aibody['catalog/filter'] ?? '' ?>
+    <?= $aibody['catalog/search'] ?? '' ?>
+    <?= $aibody['catalog/tree'] ?? '' ?>
+    <?= $aibody['catalog/price'] ?? '' ?>
+    <?= $aibody['catalog/supplier'] ?? '' ?>
+    <?= $aibody['catalog/attribute'] ?? '' ?>
 @stop
 
 @section('aimeos_body')
-     <?= $aibody['catalog/lists'] ?>
+     <?= $aibody['catalog/lists'] ?? '' ?>
 @stop
