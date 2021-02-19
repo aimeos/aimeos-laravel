b771b5ef4602a8cbb8184e9dedb49cac9f93e355
Andreas <andi@aimeos.org>
2021-02-19 13:14:30 +0100

    Removed cms page test

--- tests/Controller/PageControllerTest.php
+++ tests/Controller/PageControllerTest.php
@@ -2,15 +2,6 @@
 
 class PageControllerTest extends AimeosTestAbstract
 {
-	public function testIndexAction()
-	{
-		View::addLocation( dirname( __DIR__ ) . '/fixtures/views' );
-
-		$this->action( 'GET', '\Aimeos\Shop\Controller\PageController@indexAction', ['site' => 'unittest', 'path' => 'contact'] );
-		$this->assertResponseOk();
-	}
-
-
 	public function testPrivacyAction()
 	{
 		View::addLocation( dirname( __DIR__ ) . '/fixtures/views' );
