fd3fde23570c35728c638396b9e1429b449e5666
Andreas <andi@aimeos.org>
2021-03-10 16:50:09 +0100

    Use Bootstrap 5-beta2 and include CSS depending on language

--- src/views/jqadm/index.blade.php
+++ src/views/jqadm/index.blade.php
@@ -12,7 +12,11 @@
 
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4/css/font-awesome.min.css" />
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css,npm/flatpickr@4/dist/flatpickr.min.css,npm/flatpickr@4/dist/plugins/confirmDate/confirmDate.min.css,npm/vue-select@3/dist/vue-select.min.css">
+		@if( in_array( $lang = Request::get( 'lang', config( 'app.locale', 'en' ) ), ['ar', 'he'] ) )
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.rtl.min.css,npm/flatpickr@4/dist/flatpickr.min.css,npm/flatpickr@4/dist/plugins/confirmDate/confirmDate.min.css,npm/vue-select@3/dist/vue-select.min.css">
+		@else
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css,npm/flatpickr@4/dist/flatpickr.min.css,npm/flatpickr@4/dist/plugins/confirmDate/confirmDate.min.css,npm/vue-select@3/dist/vue-select.min.css">
+		@endif
 		<link rel="stylesheet" href="<?= route( 'aimeos_shop_jqadm_file', array( 'site' => $site, 'lang' => 'en', 'type' => 'css' ) ); ?>" />
 	</head>
 	<body>
@@ -28,7 +32,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/combine/npm/vue@2/dist/vue.min.js,npm/vue-select@3/dist/vue-select.min.js,npm/flatpickr@4,npm/flatpickr@4/dist/l10n/index.min.js,npm/flatpickr@4/dist/plugins/confirmDate/confirmDate.min.js,npm/vue-flatpickr-component@8,npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js,npm/sortablejs@1,npm/vuedraggable@2"></script>
+		<script src="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js,npm/vue@2/dist/vue.min.js,npm/vue-select@3/dist/vue-select.min.js,npm/flatpickr@4,npm/flatpickr@4/dist/l10n/index.min.js,npm/flatpickr@4/dist/plugins/confirmDate/confirmDate.min.js,npm/vue-flatpickr-component@8,npm/sortablejs@1,npm/vuedraggable@2"></script>
 		<script src="https://cdn.jsdelivr.net/npm/ckeditor@4/ckeditor.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/ckeditor@4/adapters/jquery.js"></script>
 		<script src="<?= route( 'aimeos_shop_jqadm_file', array( 'site' => $site, 'lang' => 'en', 'type' => 'js' ) ); ?>"></script>
