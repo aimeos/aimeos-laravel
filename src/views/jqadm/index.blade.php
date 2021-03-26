a26fc749586efda5ea9888452150b59381e4e1ff
Andreas <andi@aimeos.org>
2021-03-26 11:33:37 +0100

    Added switch for dark mode in admin backend

--- src/views/jqadm/index.blade.php
+++ src/views/jqadm/index.blade.php
@@ -18,13 +18,35 @@
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css,npm/flatpickr@4/dist/flatpickr.min.css,npm/flatpickr@4/dist/plugins/confirmDate/confirmDate.min.css,npm/vue-select@3/dist/vue-select.min.css">
 		@endif
 		<link rel="stylesheet" href="<?= route( 'aimeos_shop_jqadm_file', array( 'site' => $site, 'lang' => 'en', 'type' => 'css' ) ); ?>" />
+		<style>
+			body.dark .btn-theme.dark-mode {display:none}
+			body.light .btn-theme.light-mode {display:none}
+			.app-menu .icon {vertical-align: middle; padding: 0.5rem 1rem; font-size: 125%}
+		</style>
 	</head>
-	<body>
+	<body class="{{ ($_COOKIE['aimeos_backend_theme'] ?? '') == 'dark' ? 'dark' : 'light' }}">
 		<div class="app-menu">
 			<span class="menu"></span>
-			<i class="logout fa fa-sign-out" aria-hidden="true" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"></i>
-			<form id="logout-form" action="{{ url('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+			<div class="app-menu-end">
+				<i class="icon btn-theme light-mode fa fa-sun-o"></i><i class="icon btn-theme dark-mode fa fa-moon-o"></i>
+				<i class="icon logout fa fa-sign-out" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"></i>
+				<form id="logout-form" action="{{ url('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+			</div>
 		</div>
+		<script>
+			const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+			if (prefersDark.matches && !document.cookie.includes('aimeos_backend_theme=light')) {
+				['light', 'dark'].map(cl => document.body.classList.toggle(cl));
+			}
+
+			document.querySelectorAll(".btn-theme").forEach(item => {
+				item.addEventListener("click", function() {
+					['light', 'dark'].map(cl => document.body.classList.toggle(cl));
+					const theme = document.body.classList.contains("dark") ? "dark" : "light";
+					document.cookie = "aimeos_backend_theme=" + theme + ";path=/";
+				});
+			});
+		</script>
 
 <?= $content ?>
 
