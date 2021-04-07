cacab25fb924f4bcfa39d79983b2a5885591494c
Andreas <andi@aimeos.org>
2021-04-07 10:37:13 +0200

    Added basket/mini component to CMS pages

--- src/views/page/index.blade.php
+++ src/views/page/index.blade.php
@@ -1,9 +1,11 @@
 @extends('shop::base')
 
 @section('aimeos_header')
+	<?= $aiheader['basket/mini'] ?? '' ?>
     <?= $aiheader['cms/page'] ?? '' ?>
 @stop
 
 @section('aimeos_body')
+	<?= $aibody['basket/mini'] ?? '' ?>
 	<?= $aibody['cms/page'] ?? '' ?>
 @stop
