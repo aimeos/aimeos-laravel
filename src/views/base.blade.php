c511b243ee82899039625334e0c87a08a0e7d3c7
Andreas <andi@aimeos.org>
2021-08-17 19:01:23 +0200

    Added CSP nonce support

--- src/views/base.blade.php
+++ src/views/base.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="Content-Security-Policy" content="default-src 'self' https://cdn.jsdelivr.net; style-src 'unsafe-inline' 'self' https://cdn.jsdelivr.net; img-src 'self' data: https://cdn.jsdelivr.net https://aimeos.org">
+		<meta http-equiv="Content-Security-Policy" content="default-src 'self' 'nonce-{{ app( 'aimeos.context' )->get()->nonce() }}' https://cdn.jsdelivr.net; style-src 'unsafe-inline' 'self' https://cdn.jsdelivr.net; img-src 'self' data: https://cdn.jsdelivr.net https://aimeos.org">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 
 		@yield('aimeos_header')
