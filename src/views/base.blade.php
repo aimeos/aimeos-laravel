2cb9d2618a8bdab366c6d50f0751941ad083f944
Andreas <andi@aimeos.org>
2021-10-07 09:09:07 +0200

    Use default logos

--- src/views/base.blade.php
+++ src/views/base.blade.php
@@ -11,7 +11,7 @@
 
 		<title>{{ config('app.name', 'Aimeos') }}</title>
 
-		<link rel="icon" href="{{ asset( 'aimeos/' . app( 'aimeos.context' )->get()->getLocale()->getSiteItem()->getIcon() ) }}"/>
+		<link rel="icon" href="{{ asset( 'aimeos/' . ( app( 'aimeos.context' )->get()->getLocale()->getSiteItem()->getIcon() ?: '../vendor/shop/themes/default/media/aimeos-icon.png' ) ) }}"/>
 
 		<link rel="preload" href="/vendor/shop/themes/default/fonts/roboto-condensed-v19-latin/roboto-condensed-v19-latin-regular.woff2" as="font" type="font/woff2" crossorigin>
 		<link rel="preload" href="/vendor/shop/themes/default/fonts/roboto-condensed-v19-latin/roboto-condensed-v19-latin-700.woff2" as="font" type="font/woff2" crossorigin>
@@ -26,7 +26,7 @@
 	<body>
 		<nav class="navbar navbar-expand-md navbar-light navbar-top">
 			<a class="navbar-brand" href="/" title="Logo">
-				<img src="{{ asset( 'aimeos/' . app( 'aimeos.context' )->get()->getLocale()->getSiteItem()->getLogo() ) }}" height="40" title="Logo">
+				<img src="{{ asset( 'aimeos/' . ( app( 'aimeos.context' )->get()->getLocale()->getSiteItem()->getLogo() ?: '../vendor/shop/themes/default/media/aimeos.png' ) ) }}" height="40" title="Logo">
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -87,7 +87,7 @@
 					<div class="col-md-4 footer-right">
 						<div class="footer-block">
 							<a class="logo" href="/" title="Logo">
-							    <img src="{{ asset( 'aimeos/' . app( 'aimeos.context' )->get()->getLocale()->getSiteItem()->getLogo() ) }}" height="40" title="Logo">
+							    <img src="{{ asset( 'aimeos/' . ( app( 'aimeos.context' )->get()->getLocale()->getSiteItem()->getLogo() ?: '../vendor/shop/themes/default/media/aimeos.png' ) ) }}" height="40" title="Logo">
 							</a>
 							<div class="social">
 								<p><i class="bi">facebook</i><a href="#" class="sm facebook" title="Facebook" rel="noopener">Facebook</a></p>
