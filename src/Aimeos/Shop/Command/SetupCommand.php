dea5f285c8ddfe874ecb019dcad1700a6004f918
Andreas <andi@aimeos.org>
2021-11-01 08:35:28 +0100

    Use new Aimeos setup class

--- src/Aimeos/Shop/Command/SetupCommand.php
+++ src/Aimeos/Shop/Command/SetupCommand.php
@@ -29,8 +29,9 @@ class SetupCommand extends AbstractCommand
 	protected $signature = 'aimeos:setup
 		{site=default : Site for updating database entries}
 		{tplsite=default : Site used as template for creating the new one}
-		{--task= : Name of the setup task that should be executed}
-		{--option= : Setup configuration, name and value are separated by ":" like "setup/default/demo:1"}
+		{--q : Quiet}
+		{--v=vv : Verbosity level}
+		{--option= : Setup configuration, name and value are separated by colon like "setup/default/demo:1"}
 	';
 
 	/**
@@ -48,67 +49,38 @@ class SetupCommand extends AbstractCommand
 	 */
 	public function handle()
 	{
-		$ctx = $this->getLaravel()->make( 'aimeos.context' )->get( false, 'command' );
-		$ctx->setEditor( 'aimeos:setup' );
-
-		$config = $ctx->getConfig();
-		$site = $this->argument( 'site' );
-		$template = $this->argument( 'tplsite' );
-
-		$task = $this->option( 'task' );
-
-		$config->set( 'setup/site', $site );
-		$dbconfig = $this->getDbConfig( $config );
-		$this->setOptions( $config );
-
 		\Aimeos\MShop::cache( false );
 		\Aimeos\MAdmin::cache( false );
 
-		$taskPaths = $this->getLaravel()->make( 'aimeos' )->get()->getSetupPaths( $template );
-		$manager = new \Aimeos\MW\Setup\Manager\Multiple( $ctx->getDatabaseManager(), $dbconfig, $taskPaths, $ctx );
+		$site = $this->argument( 'site' );
+		$template = $this->argument( 'tplsite' );
+
+		$boostrap = $this->getLaravel()->make( 'aimeos' )->get();
+		$ctx = $this->getLaravel()->make( 'aimeos.context' )->get( false, 'command' )->setEditor( 'aimeos:setup' );
 
 		$this->info( sprintf( 'Initializing or updating the Aimeos database tables for site "%1$s"', $site ) );
 
-		$manager->migrate( $task );
+		\Aimeos\Setup::use( $boostrap, $this->getOptions() )
+			->verbose( $this->option( 'q' ) ? '' : $this->option( 'v' ) )
+			->up( $site, $template );
 	}
 
 
 	/**
-	 * Returns the database configuration from the config object.
+	 * Returns the configuration options from the input object
 	 *
-	 * @param \Aimeos\MW\Config\Iface $conf Config object
-	 * @return array Multi-dimensional associative list of database configuration parameters
+	 * @return array Associative list of key/value pairs of configuration options
 	 */
-	protected function getDbConfig( \Aimeos\MW\Config\Iface $conf ) : array
+	protected function getOptions() : array
 	{
-		$dbconfig = $conf->get( 'resource', array() );
+		$map = [];
 
-		foreach( $dbconfig as $rname => $dbconf )
-		{
-			if( strncmp( $rname, 'db', 2 ) !== 0 ) {
-				unset( $dbconfig[$rname] );
-			} else {
-				$conf->set( 'resource/' . $rname . '/limit', 5 );
-			}
-		}
-
-		return $dbconfig;
-	}
-
-
-	/**
-	 * Extracts the configuration options from the input object and updates the configuration values in the config object.
-	 *
-	 * @param \Aimeos\MW\Config\Iface $conf Configuration object
-	 * @param array Associative list of database configurations
-	 * @throws \RuntimeException If the format of the options is invalid
-	 */
-	protected function setOptions( \Aimeos\MW\Config\Iface $conf )
-	{
 		foreach( (array) $this->option( 'option' ) as $option )
 		{
 			list( $name, $value ) = explode( ':', $option );
-			$conf->set( str_replace( '\\', '/', $name ), $value );
+			$map[str_replace( '\\', '/', $name )] = $value;
 		}
+
+		return $map;
 	}
 }
