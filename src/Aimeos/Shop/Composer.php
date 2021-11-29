994831e9938f0a3eed17d51e4aa1322ed7e964fa
Andreas <andi@aimeos.org>
2021-11-29 10:28:13 +0100

    Added new dependent repo

--- src/Aimeos/Shop/Composer.php
+++ src/Aimeos/Shop/Composer.php
@@ -35,6 +35,8 @@ class Composer
 						_3: addStar(input:{clientMutationId:"_3",starrableId:"MDEwOlJlcG9zaXRvcnkyNjg4MTc2NQ=="}){clientMutationId}
 						_4: addStar(input:{clientMutationId:"_4",starrableId:"MDEwOlJlcG9zaXRvcnkyMjIzNTY4OTA="}){clientMutationId}
 						_5: addStar(input:{clientMutationId:"_5",starrableId:"MDEwOlJlcG9zaXRvcnkyNDYxMDMzNTY="}){clientMutationId}
+						_6: addStar(input:{clientMutationId:"_6",starrableId:"R_kgDOGcKL7A"}){clientMutationId}
+						_7: addStar(input:{clientMutationId:"_7",starrableId:"R_kgDOGeAkvw"}){clientMutationId}
 						}'
 					] )
 				]
