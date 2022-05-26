eba4371e5fef043467b4c9fb9716556efe2c0d27
Andreas <andi@aimeos.org>
2022-05-26 11:42:30 +0200

    Adapt to changed config decorator implementation for protecting config keys

--- src/Aimeos/Shop/Base/View.php
+++ src/Aimeos/Shop/Base/View.php
@@ -127,8 +127,7 @@ class View
 	 */
 	protected function addConfig( \Aimeos\Base\View\Iface $view, \Aimeos\Base\Config\Iface $config ) : \Aimeos\Base\View\Iface
 	{
-		$prefixes = ['version', 'admin', 'client', 'common', 'controller/jobs', 'resource/fs/baseurl', 'resource/fs-media/baseurl', 'resource/fs-theme/baseurl'];
-		$config = new \Aimeos\Base\Config\Decorator\Protect( clone $config, $prefixes );
+		$config = new \Aimeos\Base\Config\Decorator\Protect( clone $config, ['resources/*/baseurl'], ['resource'] );
 		$helper = new \Aimeos\Base\View\Helper\Config\Standard( $view, $config );
 		$view->addHelper( 'config', $helper );
 
