8d1915244b70716e8a34057335f1b8ad0a23d3a4
Andreas <andi@aimeos.org>
2022-05-27 10:10:46 +0200

    Fixed locale paramter for admin login redirect

--- src/Aimeos/Shop/Controller/AdminController.php
+++ src/Aimeos/Shop/Controller/AdminController.php
@@ -40,7 +40,7 @@ class AdminController extends Controller
 		if( Auth::check() === false
 			|| $request->user()->can( 'admin', [AdminController::class, ['admin', 'editor']] ) === false
 		) {
-			return redirect()->guest( route( 'login', app()->getLocale() ) );
+			return redirect()->guest( airoute( 'login', ['locale' => app()->getLocale()] ) );
 		}
 
 		$context = app( 'aimeos.context' )->get( false );
