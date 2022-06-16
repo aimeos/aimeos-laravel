e034d52a9cb49dd1f4e2518ee81487115c2f2182
Andreas <andi@aimeos.org>
2022-06-16 11:59:59 +0200

    Add complete resource configuration to shop.php

--- src/config/shop.php
+++ src/config/shop.php
@@ -23,47 +23,93 @@ return [
 		// 'update' => [],
 	],
 
-	/*
 	'page' => [
-		// Docs: https://aimeos.org/docs/latest/laravel/extend/#adapt-pages
-		'account-index' => [ 'locale/select', 'basket/mini','catalog/tree','catalog/search','account/profile','account/review','account/subscription','account/history','account/favorite','account/watch','catalog/session' ],
-		'basket-index' => [ 'locale/select', 'catalog/tree','catalog/search','basket/standard','basket/bulk','basket/related' ],
-		'catalog-count' => [ 'catalog/count' ],
-		'catalog-detail' => [ 'locale/select', 'basket/mini','catalog/tree','catalog/search','catalog/stage','catalog/detail','catalog/session' ],
-		'catalog-home' => [ 'locale/select','basket/mini','catalog/tree','catalog/search','catalog/home', 'cms/page' ],
-		'catalog-list' => [ 'locale/select','basket/mini','catalog/filter','catalog/tree','catalog/search','catalog/price','catalog/supplier','catalog/attribute','catalog/session','catalog/stage','catalog/lists' ],
-		'catalog-session' => [ 'locale/select','basket/mini','catalog/tree','catalog/search','catalog/session' ],
-		'catalog-stock' => [ 'catalog/stock' ],
-		'catalog-suggest' => [ 'catalog/suggest' ],
-		'catalog-tree' => [ 'locale/select','basket/mini','catalog/filter','catalog/tree','catalog/search','catalog/price','catalog/supplier','catalog/attribute','catalog/session','catalog/stage','catalog/lists' ],
-		'checkout-confirm' => [ 'catalog/tree','catalog/search','checkout/confirm' ],
-		'checkout-index' => [ 'locale/select', 'catalog/tree','catalog/search','checkout/standard' ],
-		'checkout-update' => [ 'checkout/update' ],
-		'supplier-detail' => [ 'locale/select','basket/mini','catalog/tree','catalog/search','supplier/detail','catalog/lists'],
-		'cms' => [ 'basket/mini','catalog/tree','cms/page' ],
+		'account-index' => ['locale/select', 'basket/mini', 'catalog/tree', 'catalog/search', 'account/profile', 'account/review', 'account/subscription', 'account/history', 'account/favorite', 'account/watch', 'catalog/session'],
+		'basket-index' => ['locale/select', 'catalog/tree', 'catalog/search', 'basket/standard', 'basket/bulk', 'basket/related'],
+		'catalog-count' => ['catalog/count'],
+		'catalog-detail' => ['locale/select', 'basket/mini', 'catalog/tree', 'catalog/search', 'catalog/stage', 'catalog/detail', 'catalog/session'],
+		'catalog-home' => ['locale/select', 'basket/mini', 'catalog/tree', 'catalog/search', 'catalog/home'],
+		'catalog-list' => ['locale/select', 'basket/mini', 'catalog/filter', 'catalog/tree', 'catalog/search', 'catalog/price', 'catalog/supplier', 'catalog/attribute', 'catalog/session', 'catalog/stage', 'catalog/lists'],
+		'catalog-session' => ['locale/select', 'basket/mini', 'catalog/tree', 'catalog/search', 'catalog/session'],
+		'catalog-stock' => ['catalog/stock'],
+		'catalog-suggest' => ['catalog/suggest'],
+		'catalog-tree' => ['locale/select', 'basket/mini', 'catalog/filter', 'catalog/tree', 'catalog/search', 'catalog/price', 'catalog/supplier', 'catalog/attribute', 'catalog/session', 'catalog/stage', 'catalog/lists'],
+		'checkout-confirm' => ['catalog/tree', 'catalog/search', 'checkout/confirm'],
+		'checkout-index' => ['locale/select', 'catalog/tree', 'catalog/search', 'checkout/standard'],
+		'checkout-update' => ['checkout/update'],
+		'supplier-detail' => ['locale/select', 'basket/mini', 'catalog/tree', 'catalog/search', 'supplier/detail', 'catalog/lists'],
 	],
-	*/
 
-	/*
 	'resource' => [
 		'db' => [
-			'adapter' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.driver', 'mysql'),
-			'host' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.host', '127.0.0.1'),
-			'port' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.port', '3306'),
-			'socket' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.unix_socket', ''),
-			'database' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.database', 'forge'),
-			'username' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.username', 'forge'),
-			'password' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.password', ''),
+			'adapter' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.driver', 'mysql' ),
+			'host' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.host', '127.0.0.1' ),
+			'port' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.port', '3306' ),
+			'socket' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.unix_socket', '' ),
+			'database' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.database', 'forge' ),
+			'username' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.username', 'forge' ),
+			'password' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.password', '' ),
 			'stmt' => config( 'database.default', 'mysql' ) === 'mysql' ? ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8mb4'; SET SESSION sql_mode='ANSI'"] : [],
 			'limit' => 3, // maximum number of concurrent database connections
 			'defaultTableOptions' => [
-					'charset' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.charset'),
-					'collate' => config('database.connections.' . config( 'database.default', 'mysql' ) . '.collation'),
+				'charset' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.charset' ),
+				'collate' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.collation' ),
 			],
 			'driverOptions' => config( 'database.connections.' . config( 'database.default', 'mysql' ) . '.options' ),
 		],
+		'fs' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => public_path( 'aimeos' ),
+			'baseurl' => rtrim(env('ASSET_URL', PHP_SAPI == 'cli' ? env('APP_URL') : '', '/')) . '/aimeos',
+		],
+		'fs-media' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => public_path( 'aimeos' ),
+			'baseurl' => rtrim(env('ASSET_URL', PHP_SAPI == 'cli' ? env('APP_URL') : '', '/')) . '/aimeos',
+		],
+		'fs-mimeicon' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => public_path( 'vendor/shop/mimeicons' ),
+			'baseurl' => rtrim(env('ASSET_URL', PHP_SAPI == 'cli' ? env('APP_URL') : '', '/')) . '/vendor/shop/mimeicons',
+		],
+		'fs-theme' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => public_path( 'vendor/shop/themes' ),
+			'baseurl' => rtrim(env('ASSET_URL', PHP_SAPI == 'cli' ? env('APP_URL') : '', '/')) . '/vendor/shop/themes',
+		],
+		'fs-admin' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => storage_path( 'admin' ),
+		],
+		'fs-export' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => storage_path( 'export' ),
+		],
+		'fs-import' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => storage_path( 'import' ),
+		],
+		'fs-secure' => [
+			'adapter' => 'Standard',
+			'tempdir' => storage_path( 'tmp' ),
+			'basedir' => storage_path( 'secure' ),
+		],
+		'mq' => [
+			'adapter' => 'Standard',
+			'db' => 'db',
+		],
+		'email' => [
+			'from-email' => config( 'mail.from.address' ),
+			'from-name' => config( 'mail.from.name' ),
+		],
 	],
-	*/
 
 	'admin' => [],
 
