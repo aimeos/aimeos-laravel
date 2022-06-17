e8dd1b3c7f5f75bfe0fcf8330f4eee5efffb0fca
Andreas <andi@aimeos.org>
2022-06-17 19:06:18 +0200

    Don't explicitely configure sub-manager names

--- src/default.php
+++ src/default.php
@@ -351,15 +351,6 @@ return [
 		'customer' => [
 			'manager' => [
 				'name' => 'Laravel',
-				'address' => [
-					'name' => 'Laravel',
-				],
-				'lists' => [
-					'name' => 'Laravel',
-				],
-				'property' => [
-					'name' => 'Laravel',
-				],
 				'password' => [
 					'name' => 'Bcrypt',
 				],
@@ -368,21 +359,6 @@ return [
 		'index' => [
 			'manager' => [
 				'name' => $aimeosIndexManagerName,
-				'attribute' => [
-					'name' => $aimeosIndexManagerName,
-				],
-				'catalog' => [
-					'name' => $aimeosIndexManagerName,
-				],
-				'price' => [
-					'name' => $aimeosIndexManagerName,
-				],
-				'supplier' => [
-					'name' => $aimeosIndexManagerName,
-				],
-				'text' => [
-					'name' => $aimeosIndexManagerName,
-				],
 			],
 		],
 	],
