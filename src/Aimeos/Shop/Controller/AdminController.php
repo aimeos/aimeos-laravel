030a51709d86f70d82dce0013e40fce837505d89
Andreas <andi@aimeos.org>
2023-01-12 15:51:19 +0100

    Revert "Removed locale parameter for login route"
    This reverts commit 95a1a494728fdc55ae850253bb3c03ddf29fe90b.

--- src/Aimeos/Shop/Controller/AdminController.php
+++ src/Aimeos/Shop/Controller/AdminController.php
@@ -40,7 +40,7 @@ class AdminController extends Controller
 		if( Auth::check() === false
 			|| $request->user()->can( 'admin', [AdminController::class, ['admin', 'editor']] ) === false
 		) {
-			return redirect()->guest( airoute( 'login' ) );
+			return redirect()->guest( airoute( 'login', ['locale' => app()->getLocale()] ) );
 		}
 
 		$context = app( 'aimeos.context' )->get( false );
