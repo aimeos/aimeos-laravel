7f4cbdb22e906ea9e4c27ff2d46fb38ecd041f91
Andreas <andi@aimeos.org>
2023-11-06 11:15:47 +0100

    Fixed tests

--- tests/Base/SupportTest.php
+++ tests/Base/SupportTest.php
@@ -13,18 +13,4 @@ class SupportTest extends AimeosTestAbstract
 
 		$this->assertFalse( $object->checkUserGroup( $user, 'admin' ) );
 	}
-
-
-	public function testGetGroups()
-	{
-		$context = $this->app->make( '\Aimeos\Shop\Base\Context' );
-		$locale = $this->app->make( '\Aimeos\Shop\Base\Locale' );
-
-		$ctx = $context->get( false );
-		$ctx->setLocale( $locale->getBackend( $ctx, 'unittest' ) );
-
-		$object = new \Aimeos\Shop\Base\Support( $context, $locale );
-
-		$this->assertEquals( array(), $object->getGroups( $ctx ) );
-	}
 }
