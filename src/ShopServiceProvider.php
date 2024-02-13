18fda45a22ab99d7fbc9bf0c9094a0d46f49c066
Andreas <andi@aimeos.org>
2024-02-13 10:07:58 +0100

    Improved publishing themes

--- src/ShopServiceProvider.php
+++ src/ShopServiceProvider.php
@@ -39,28 +39,15 @@ class ShopServiceProvider extends ServiceProvider {
 		$this->publishes( [dirname( __DIR__ ) . '/config/shop.php' => config_path( 'shop.php' )], 'config' );
 		$this->publishes( [dirname( __DIR__ ) . '/public' => public_path( 'vendor/shop' )], 'public' );
 
-		if( file_exists( $basepath = base_path( 'ext' ) ) )
-		{
-			foreach( new \DirectoryIterator( $basepath ) as $entry )
-			{
-				if( $entry->isDir() && !$entry->isDot() && file_exists( $entry->getPathName() . '/themes/client/html' ) ) {
-					$this->publishes( [$entry->getPathName() . '/themes/client/html/' => public_path( 'vendor/shop/themes' )], 'public' );
-				}
-			}
-		}
-
 		$class = '\Composer\InstalledVersions';
 
 		if( class_exists( $class ) && method_exists( $class, 'getInstalledPackagesByType' ) )
 		{
-			$extdir = base_path( 'ext' );
-			$packages = \Composer\InstalledVersions::getInstalledPackagesByType( 'aimeos-extension' );
-
-			foreach( $packages as $package )
+			foreach( \Composer\InstalledVersions::getInstalledPackagesByType( 'aimeos-extension' ) as $package )
 			{
 				$path = realpath( \Composer\InstalledVersions::getInstallPath( $package ) );
 
-				if( strncmp( $path, $extdir, strlen( $extdir ) ) && file_exists( $path . '/themes/client/html' ) ) {
+				if( file_exists( $path . '/themes/client/html' ) ) {
 					$this->publishes( [$path . '/themes/client/html' => public_path( 'vendor/shop/themes' )], 'public' );
 				}
 			}
