5adeb1233d56270c4d1b0ec0a6b3bb208e608ea6
Andreas <andi@aimeos.org>
2025-05-27 13:15:17 +0200

    Removed shrink-to-fit=no in admin panel template

--- views/admin/index.blade.php
+++ views/admin/index.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Aimeos administration interface</title>
 
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4/css/font-awesome.min.css">
